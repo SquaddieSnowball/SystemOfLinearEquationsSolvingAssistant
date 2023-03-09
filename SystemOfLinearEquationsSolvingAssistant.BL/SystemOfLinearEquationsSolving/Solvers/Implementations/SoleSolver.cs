24c66f4a5eae36b72787b81a247c8bd38554393d
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-03-09 18:54:42 +0300

    feat(bl): add SOLE solver implementation

---
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/Solvers/Implementations/SoleSolver.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.SolvingAlgorithms;
+
+namespace SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Solvers.Implementations;
+
+public sealed class SoleSolver : ISoleSolver
+{
+    public SoleSolvingResults SolveSerial(Sole sole, SoleSolvingAlgorithmSerial solvingAlgorithm)
+    {
+        Func<Sole, double[]> solvingAlgorithmMethod;
+
+        try
+        {
+            solvingAlgorithmMethod = SoleSolvingAlgorithmsSerial.GetAlgorithmMethod(solvingAlgorithm);
+        }
+        catch
+        {
+            throw;
+        }
+
+        Stopwatch stopwatch = new();
+        double[] solutionSet;
+
+        try
+        {
+            stopwatch.Start();
+            solutionSet = solvingAlgorithmMethod(sole);
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            stopwatch.Stop();
+        }
+
+        return new SoleSolvingResults(solutionSet, stopwatch.Elapsed);
+    }
+
+    public SoleSolvingResults SolveParallel(Sole sole, SoleSolvingAlgorithmParallel solvingAlgorithm, int numberOfThreads)
+    {
+        Func<Sole, int, double[]> solvingAlgorithmMethod;
+
+        try
+        {
+            solvingAlgorithmMethod = SoleSolvingAlgorithmsParallel.GetAlgorithmMethod(solvingAlgorithm);
+        }
+        catch
+        {
+            throw;
+        }
+
+        Stopwatch stopwatch = new();
+        double[] solutionSet;
+
+        try
+        {
+            stopwatch.Start();
+            solutionSet = solvingAlgorithmMethod(sole, numberOfThreads);
+        }
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            stopwatch.Stop();
+        }
+
+        return new SoleSolvingResults(solutionSet, stopwatch.Elapsed);
+    }
+}
