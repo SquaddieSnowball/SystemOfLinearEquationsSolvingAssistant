04ba1a3024470e0b380ba01c2899fb6b95c6da3f
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-03-09 18:53:30 +0300

    feat(bl): add SOLE solver interface

---
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/Solvers/ISoleSolver.cs
@@ -0,0 +1,10 @@
+﻿using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+
+namespace SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Solvers;
+
+public interface ISoleSolver
+{
+    SoleSolvingResults SolveSerial(Sole sole, SoleSolvingAlgorithmSerial solvingAlgorithm);
+
+    SoleSolvingResults SolveParallel(Sole sole, SoleSolvingAlgorithmParallel solvingAlgorithm, int numberOfThreads);
+}
