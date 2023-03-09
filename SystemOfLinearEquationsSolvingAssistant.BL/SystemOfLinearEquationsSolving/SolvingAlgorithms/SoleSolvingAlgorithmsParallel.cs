dc0e3ef7f6193b289e310278bf046034bdb9324d
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-03-09 18:52:23 +0300

    feat(bl): add containers for SOLE solution algorithms

---
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/SolvingAlgorithms/SoleSolvingAlgorithmsParallel.cs
@@ -0,0 +1,12 @@
+﻿using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+
+namespace SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.SolvingAlgorithms;
+
+internal static class SoleSolvingAlgorithmsParallel
+{
+    internal static Func<Sole, int, double[]> GetAlgorithmMethod(SoleSolvingAlgorithmParallel solvingAlgorithm) =>
+        solvingAlgorithm switch
+        {
+            _ => throw new NotImplementedException("This solving algorithm has not yet been implemented.")
+        };
+}
---
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/SolvingAlgorithms/SoleSolvingAlgorithmsSerial.cs
@@ -0,0 +1,12 @@
+﻿using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+
+namespace SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.SolvingAlgorithms;
+
+internal static class SoleSolvingAlgorithmsSerial
+{
+    internal static Func<Sole, double[]> GetAlgorithmMethod(SoleSolvingAlgorithmSerial solvingAlgorithm) =>
+        solvingAlgorithm switch
+        {
+            _ => throw new NotImplementedException("This solving algorithm has not yet been implemented.")
+        };
+}
