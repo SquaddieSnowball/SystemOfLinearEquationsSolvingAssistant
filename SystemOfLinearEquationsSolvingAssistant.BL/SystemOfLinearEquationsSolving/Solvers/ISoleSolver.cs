7b606a47e3ff68f333f9225316bed895b4b7d2a5
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-03-22 18:10:08 +0300

    feat(bl): add async methods to SOLE solver interface

--- SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/Solvers/ISoleSolver.cs
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/Solvers/ISoleSolver.cs
@@ -7,4 +7,8 @@ public interface ISoleSolver
     SoleSolvingResults SolveSerial(Sole sole, SoleSolvingAlgorithmSerial solvingAlgorithm);
 
     SoleSolvingResults SolveParallel(Sole sole, SoleSolvingAlgorithmParallel solvingAlgorithm, int numberOfThreads);
+
+    Task<SoleSolvingResults> SolveSerialAsync(Sole sole, SoleSolvingAlgorithmSerial solvingAlgorithm);
+
+    Task<SoleSolvingResults> SolveParallelAsync(Sole sole, SoleSolvingAlgorithmParallel solvingAlgorithm, int numberOfThreads);
 }
