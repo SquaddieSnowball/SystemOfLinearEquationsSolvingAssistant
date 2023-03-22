1cad5e3c06ee876e16df25c2ee941ab5ef4cb1ca
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-03-22 18:00:19 +0300

    feat(bl): add a class containing thread synchronization primitive extensions

---
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/SolvingAlgorithms/Helpers/ThreadSynchronizationPrimitiveExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.SolvingAlgorithms.Helpers;
+
+internal static class ThreadSynchronizationPrimitiveExtensions
+{
+    internal static void ParticipantsRemainingWait(this Barrier barrier, int participantCount)
+    {
+        while (barrier.ParticipantsRemaining != participantCount) { }
+    }
+}
