a0fa1e78db601d07a39d8255f8f5e10fb3b6aa94
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-03-27 19:06:01 +0300

    feat(bl): add a SOLE parser interface

---
+++ SystemOfLinearEquationsSolvingAssistant.BL/SystemOfLinearEquationsSolving/Parsers/ISoleParser.cs
@@ -0,0 +1,13 @@
+﻿using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Parsers.Entities;
+
+namespace SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Parsers;
+
+public interface ISoleParser
+{
+    void Initialize(string initializationString);
+
+    Sole Parse(string pathA, string pathB, SoleParsingTemplate parsingTemplate);
+
+    Task<Sole> ParseAsync(string pathA, string pathB, SoleParsingTemplate parsingTemplate);
+}
