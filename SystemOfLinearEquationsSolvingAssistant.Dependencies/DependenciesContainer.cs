a56b9ca410cd8375ea6044925a0fc50435e17d99
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-04-04 19:14:10 +0300

    feat(dependencies): add a dependency container

---
+++ SystemOfLinearEquationsSolvingAssistant.Dependencies/DependenciesContainer.cs
@@ -0,0 +1,39 @@
+﻿namespace SystemOfLinearEquationsSolvingAssistant.Dependencies;
+
+public static class DependenciesContainer
+{
+    private static readonly Dictionary<Type, Type> _dependencies = new();
+
+    public static IEnumerable<KeyValuePair<Type, Type>> GetAll() => _dependencies;
+
+    public static void Register<TAbstract, TConcrete>() where TConcrete : TAbstract
+    {
+        try
+        {
+            _dependencies.Add(typeof(TAbstract), typeof(TConcrete));
+        }
+        catch (ArgumentException)
+        {
+            throw new ArgumentException("This dependency has already been registered.");
+        }
+    }
+
+    public static T? Resolve<T>(params object?[]? ctorArgs)
+    {
+        Type type = typeof(T);
+
+        if (_dependencies.ContainsKey(type) is true)
+        {
+            try
+            {
+                return (T?)Activator.CreateInstance(_dependencies[type], ctorArgs);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        else
+            throw new ArgumentException("This dependency has not yet been registered.");
+    }
+}
