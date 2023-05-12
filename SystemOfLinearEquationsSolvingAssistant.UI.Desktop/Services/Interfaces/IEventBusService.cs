97ebbb235a742deb44553d1a04b6068bf0beb705
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:28:19 +0300

    feat (desktop): add an event bus service interface

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/Services/Interfaces/IEventBusService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Models.EventBus;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Services.Interfaces;
+
+internal interface IEventBusService
+{
+    void Subscribe<T>(Action<T> integrationEventHandler) where T : IntegrationEvent;
+
+    void Unsubscribe<T>(Action<T> integrationEventHandler) where T : IntegrationEvent;
+
+    void Publish(IntegrationEvent integrationEvent);
+}
