68aa7116217575432b86ea47af22339e55a22cec
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:28:41 +0300

    feat (desktop): add an event bus service implementation

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/Services/Implementations/EventBusService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Models.EventBus;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Services.Interfaces;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Services.Implementations;
+
+internal sealed class EventBusService : IEventBusService
+{
+    private readonly Dictionary<Type, Delegate?> _subscribtions = new();
+    private readonly object _lock = new();
+
+    public void Subscribe<T>(Action<T> integrationEventHandler) where T : IntegrationEvent
+    {
+        Type integrationEventType = typeof(T);
+
+        lock (_lock)
+        {
+            if (_subscribtions.TryGetValue(integrationEventType, out Delegate? currentIntegrationEventHandlers) is false)
+                _subscribtions.Add(integrationEventType, integrationEventHandler);
+            else
+                _subscribtions[integrationEventType] =
+                    Delegate.Combine(currentIntegrationEventHandlers, integrationEventHandler);
+        }
+    }
+
+    public void Unsubscribe<T>(Action<T> integrationEventHandler) where T : IntegrationEvent
+    {
+        Type integrationEventType = typeof(T);
+
+        lock (_lock)
+        {
+            if (_subscribtions.TryGetValue(integrationEventType, out Delegate? currentIntegrationEventHandlers) is false)
+                return;
+            else
+                _subscribtions[integrationEventType] =
+                    Delegate.Remove(currentIntegrationEventHandlers, integrationEventHandler);
+        }
+    }
+
+    public void Publish(IntegrationEvent integrationEvent)
+    {
+        if (integrationEvent is null)
+            throw new ArgumentNullException(nameof(integrationEvent), "Integration event must not be null.");
+
+        lock (_lock)
+        {
+            if (_subscribtions.TryGetValue(integrationEvent.GetType(), out Delegate? currentIntegrationEventHandlers) is false)
+                return;
+
+            _ = (currentIntegrationEventHandlers?.DynamicInvoke(integrationEvent));
+        }
+    }
+}
