51eac11019d9fdbf88a3ce9efa79929876d33eac
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:26:49 +0300

    feat (desktop): add a view manager service interface

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/Services/Interfaces/IViewManagerService.cs
@@ -0,0 +1,8 @@
+﻿namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Services.Interfaces;
+
+internal interface IViewManagerService
+{
+    void ShowView(string viewName, string? ownerViewName = default, bool isDialogMode = false);
+
+    void CloseView(string viewName);
+}
