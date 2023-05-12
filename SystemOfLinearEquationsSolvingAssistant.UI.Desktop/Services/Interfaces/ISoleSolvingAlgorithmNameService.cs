c46a5b2ea4a845737cf74be0074eb57331d81251
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:31:18 +0300

    feat (desktop): add SOLE solving algorithm name service interface

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/Services/Interfaces/ISoleSolvingAlgorithmNameService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Services.Interfaces;
+
+internal interface ISoleSolvingAlgorithmNameService
+{
+    IEnumerable<string> AlgorithmNamesSerial { get; }
+
+    IEnumerable<string> AlgorithmNamesParallel { get; }
+
+    string GetAlgorithmNameSerial(SoleSolvingAlgorithmSerial solvingAlgorithm);
+
+    string GetAlgorithmNameParallel(SoleSolvingAlgorithmParallel solvingAlgorithm);
+
+    SoleSolvingAlgorithmSerial GetAlgorithmByNameSerial(string solvingAlgorithmName);
+
+    SoleSolvingAlgorithmParallel GetAlgorithmByNameParallel(string solvingAlgorithmName);
+}
