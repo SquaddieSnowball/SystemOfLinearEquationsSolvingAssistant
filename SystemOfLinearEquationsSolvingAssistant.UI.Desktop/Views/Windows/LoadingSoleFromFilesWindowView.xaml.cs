6faa91827cbc81f8089c45c04161dde4a2b9b941
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:54:54 +0300

    feat(desktop): add a view for the loading SOLE from files

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/Views/Windows/LoadingSoleFromFilesWindowView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Views.Windows;
+
+/// <summary>
+/// Interaction logic for LoadingSoleFromFilesWindowView.xaml
+/// </summary>
+public partial class LoadingSoleFromFilesWindowView : Window
+{
+    public LoadingSoleFromFilesWindowView() => InitializeComponent();
+}
