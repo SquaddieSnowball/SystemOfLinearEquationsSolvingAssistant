f9971f0725291177c903603581ac14b681950e5c
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:55:58 +0300

    feat(desktop): add a views registrator

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/Views/ViewsRegistrator.cs
@@ -0,0 +1,14 @@
+﻿using SystemOfLinearEquationsSolvingAssistant.Dependencies.Entities;
+using SystemOfLinearEquationsSolvingAssistant.Dependencies;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Views.Windows;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Views;
+
+internal static class ViewsRegistrator
+{
+    public static void Register()
+    {
+        DependenciesContainer.Register<MainWindowView>(DependencyObjectLifetime.Singleton);
+        DependenciesContainer.Register<LoadingSoleFromFilesWindowView>(DependencyObjectLifetime.Transient);
+    }
+}
