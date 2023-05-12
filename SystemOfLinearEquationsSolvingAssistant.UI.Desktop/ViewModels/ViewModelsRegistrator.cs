317a08595cea98dcd28d714e2c833f0ca3620440
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:51:11 +0300

    feat(desktop): add a view models registrator

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/ViewModels/ViewModelsRegistrator.cs
@@ -0,0 +1,14 @@
+﻿using SystemOfLinearEquationsSolvingAssistant.Dependencies.Entities;
+using SystemOfLinearEquationsSolvingAssistant.Dependencies;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.ViewModels.Windows;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.ViewModels;
+
+internal static class ViewModelsRegistrator
+{
+    public static void Register()
+    {
+        DependenciesContainer.Register<MainWindowViewModel>(DependencyObjectLifetime.Singleton);
+        DependenciesContainer.Register<LoadingSoleFromFilesWindowViewModel>(DependencyObjectLifetime.Singleton);
+    }
+}
