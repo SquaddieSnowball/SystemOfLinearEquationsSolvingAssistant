f6035228a396e4075e20fd6414179c798e93ab74
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-05-13 00:49:17 +0300

    feat(desktop): add the view model of the main window

---
+++ SystemOfLinearEquationsSolvingAssistant.UI.Desktop/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Entities;
+using SystemOfLinearEquationsSolvingAssistant.BL.SystemOfLinearEquationsSolving.Solvers;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Commands;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Models.EventBus.IntegrationEvents;
+using SystemOfLinearEquationsSolvingAssistant.UI.Desktop.Services.Interfaces;
+
+namespace SystemOfLinearEquationsSolvingAssistant.UI.Desktop.ViewModels.Windows;
+
+internal sealed class MainWindowViewModel : ViewModel
+{
+    private const int MinDataDimension = 1;
+    private const int MaxDataDimension = 999;
+    private const int MinThreadsNumParallel = 2;
+    private const int MaxThreadsNumParallel = 99;
+
+    private readonly ISoleSolver _soleSolver;
+    private readonly IViewManagerService _viewManagerService;
+    private readonly IEventBusService _eventBusService;
+    private readonly IUserDialogService _userDialogService;
+    private readonly ISoleSolvingAlgorithmNameService _soleSolvingAlgorithmNameService;
+
+    private int _currentDataDimension;
+    private DataTable _dataTableMatrixA;
+    private DataTable _dataTableVectorB;
+    private string _solutionSet;
+    private string[] _algorithmNamesSerial;
+    private string[] _algorithmNamesParallel;
+    private int _algorithmNamesSerialSelectedIndex;
+    private int _algorithmNamesParallelSelectedIndex;
+    private int _threadsNumParallel;
+    private TimeSpan _elapsedTimeSerial;
+    private TimeSpan _elapsedTimeParallel;
+    private bool _isSolvingProcessEnded;
+
+    public DataTable DataTableMatrixA
+    {
+        get => _dataTableMatrixA;
+        set => Set(ref _dataTableMatrixA, value);
+    }
+
+    public DataTable DataTableVectorB
+    {
+        get => _dataTableVectorB;
+        set => Set(ref _dataTableVectorB, value);
+    }
+
+    public string SolutionSet
+    {
+        get => _solutionSet;
+        set => Set(ref _solutionSet, value);
+    }
+
+    public string[] AlgorithmNamesSerial
+    {
+        get => _algorithmNamesSerial;
+        set => Set(ref _algorithmNamesSerial, value);
+    }
+
+    public string[] AlgorithmNamesParallel
+    {
+        get => _algorithmNamesParallel;
+        set => Set(ref _algorithmNamesParallel, value);
+    }
+
+    public int AlgorithmNamesSerialSelectedIndex
+    {
+        get => _algorithmNamesSerialSelectedIndex;
+        set => Set(ref _algorithmNamesSerialSelectedIndex, value);
+    }
+
+    public int AlgorithmNamesParallelSelectedIndex
+    {
+        get => _algorithmNamesParallelSelectedIndex;
+        set => Set(ref _algorithmNamesParallelSelectedIndex, value);
+    }
+
+    public int ThreadsNumParallel
+    {
+        get => _threadsNumParallel;
+        set => Set(ref _threadsNumParallel, value);
+    }
+
+    public TimeSpan ElapsedTimeSerial
+    {
+        get => _elapsedTimeSerial;
+        set => Set(ref _elapsedTimeSerial, value);
+    }
+
+    public TimeSpan ElapsedTimeParallel
+    {
+        get => _elapsedTimeParallel;
+        set => Set(ref _elapsedTimeParallel, value);
+    }
+
+    public bool IsSolvingProcessEnded
+    {
+        get => _isSolvingProcessEnded;
+        set => Set(ref _isSolvingProcessEnded, value);
+    }
+
+    public ICommand AddDataDimensionCommand { get; }
+
+    public ICommand RemoveDataDimensionCommand { get; }
+
+    public ICommand ResetDataCommand { get; }
+
+    public ICommand LoadFromFilesCommand { get; }
+
+    public ICommand AddThreadParallelCommand { get; }
+
+    public ICommand RemoveThreadParallelCommand { get; }
+
+    public ICommand SolveSerialCommand { get; }
+
+    public ICommand SolveParallelCommand { get; }
+
+    public MainWindowViewModel(ISoleSolver soleSolver, IViewManagerService viewManagerService, IEventBusService eventBusService,
+        IUserDialogService userDialogService, ISoleSolvingAlgorithmNameService soleSolvingAlgorithmNameService)
+    {
+        if (soleSolver is null)
+            throw new ArgumentNullException(nameof(soleSolver),
+                "Sole solver must not be null.");
+
+        if (viewManagerService is null)
+            throw new ArgumentNullException(nameof(viewManagerService),
+                "View manager service must not be null.");
+
+        if (eventBusService is null)
+            throw new ArgumentNullException(nameof(eventBusService),
+                "Event bus service must not be null.");
+
+        if (userDialogService is null)
+            throw new ArgumentNullException(nameof(userDialogService),
+                "User dialog service must not be null.");
+
+        if (soleSolvingAlgorithmNameService is null)
+            throw new ArgumentNullException(nameof(soleSolvingAlgorithmNameService),
+                "Sole solving algorithm name service must not be null.");
+
+        _soleSolver = soleSolver;
+        _viewManagerService = viewManagerService;
+        _eventBusService = eventBusService;
+        _userDialogService = userDialogService;
+        _soleSolvingAlgorithmNameService = soleSolvingAlgorithmNameService;
+
+        _dataTableMatrixA = new DataTable();
+        _dataTableVectorB = new DataTable();
+        _solutionSet = "Click \"Solve\" to display the solution set.";
+        _algorithmNamesSerial = _soleSolvingAlgorithmNameService.AlgorithmNamesSerial.ToArray();
+        _algorithmNamesParallel = _soleSolvingAlgorithmNameService.AlgorithmNamesParallel.ToArray();
+        _threadsNumParallel = MinThreadsNumParallel;
+        _isSolvingProcessEnded = true;
+
+        AddDataDimensionCommand = new RelayCommand(OnAddDataDimensionCommandExecute, CanAddDataDimensionCommandExecute);
+        RemoveDataDimensionCommand = new RelayCommand(OnRemoveDataDimensionCommandExecute, CanRemoveDataDimensionCommandExecute);
+        ResetDataCommand = new RelayCommand(OnResetDataCommandExecute, CanResetDataCommandExecute);
+        LoadFromFilesCommand = new RelayCommand(OnLoadFromFilesCommandExecute, CanLoadFromFilesCommandExecute);
+        AddThreadParallelCommand = new RelayCommand(OnAddThreadParallelCommandExecute, CanAddThreadParallelCommandExecute);
+        RemoveThreadParallelCommand = new RelayCommand(OnRemoveThreadParallelCommandExecute, CanRemoveThreadParallelCommandExecute);
+        SolveSerialCommand = new RelayCommand(OnSolveSerialCommandExecute, CanSolveSerialCommandExecute);
+        SolveParallelCommand = new RelayCommand(OnSolveParallelCommandExecute, CanSolveParallelCommandExecute);
+
+        _eventBusService.Subscribe<SoleLoadedIntegrationEvent>(OnSoleLoadedIntegrationEvent);
+
+        ResetData();
+    }
+
+    private void OnAddDataDimensionCommandExecute() => ChangeDataDimension(1, true);
+
+    private bool CanAddDataDimensionCommandExecute() => _currentDataDimension is not MaxDataDimension;
+
+    private void OnRemoveDataDimensionCommandExecute() => ChangeDataDimension(1, false);
+
+    private bool CanRemoveDataDimensionCommandExecute() => _currentDataDimension is not MinDataDimension;
+
+    private void OnResetDataCommandExecute() => ResetData();
+
+    private bool CanResetDataCommandExecute() => true;
+
+    private void OnLoadFromFilesCommandExecute() =>
+        _viewManagerService.ShowView("LoadingSoleFromFilesWindow", "MainWindow", true);
+
+    private bool CanLoadFromFilesCommandExecute() => true;
+
+    private void OnAddThreadParallelCommandExecute() => ThreadsNumParallel++;
+
+    private bool CanAddThreadParallelCommandExecute() => ThreadsNumParallel is not MaxThreadsNumParallel;
+
+    private void OnRemoveThreadParallelCommandExecute() => ThreadsNumParallel--;
+
+    private bool CanRemoveThreadParallelCommandExecute() => ThreadsNumParallel is not MinThreadsNumParallel;
+
+    private async void OnSolveSerialCommandExecute()
+    {
+        IsSolvingProcessEnded = false;
+
+        Sole sole = GetSoleFromData();
+        SoleSolvingAlgorithmSerial solvingAlgorithm = _soleSolvingAlgorithmNameService
+            .GetAlgorithmByNameSerial(AlgorithmNamesSerial[AlgorithmNamesSerialSelectedIndex]);
+
+        SoleSolvingResults solvingResults = await _soleSolver.SolveSerialAsync(sole, solvingAlgorithm);
+
+        ElapsedTimeSerial = solvingResults.ElapsedTime;
+        SetSolutionSet(solvingResults);
+
+        IsSolvingProcessEnded = true;
+    }
+
+    private bool CanSolveSerialCommandExecute() =>
+        (AlgorithmNamesSerialSelectedIndex >= 0) && (AlgorithmNamesSerialSelectedIndex < AlgorithmNamesSerial.Length);
+
+    private async void OnSolveParallelCommandExecute()
+    {
+        IsSolvingProcessEnded = false;
+
+        Sole sole = GetSoleFromData();
+        SoleSolvingAlgorithmParallel solvingAlgorithm = _soleSolvingAlgorithmNameService
+            .GetAlgorithmByNameParallel(AlgorithmNamesParallel[AlgorithmNamesParallelSelectedIndex]);
+
+        SoleSolvingResults solvingResults = await _soleSolver.SolveParallelAsync(sole, solvingAlgorithm, ThreadsNumParallel);
+
+        ElapsedTimeParallel = solvingResults.ElapsedTime;
+        SetSolutionSet(solvingResults);
+
+        IsSolvingProcessEnded = true;
+    }
+
+    private bool CanSolveParallelCommandExecute() =>
+        (AlgorithmNamesParallelSelectedIndex >= 0) && (AlgorithmNamesParallelSelectedIndex < AlgorithmNamesParallel.Length);
+
+    private void OnSoleLoadedIntegrationEvent(SoleLoadedIntegrationEvent soleLoadedIntegrationEvent)
+    {
+        Sole sole = soleLoadedIntegrationEvent.Sole;
+
+        if (sole is null)
+            return;
+
+        if (sole.Dimension < MinDataDimension)
+        {
+            _userDialogService.ShowErrorMessage("The system of linear equations cannot be loaded because it is too small. " +
+                $"Minimum table capacity: \"{MinDataDimension}\"");
+
+            return;
+        }
+
+        if (sole.Dimension > MaxDataDimension)
+        {
+            _userDialogService.ShowErrorMessage("The system of linear equations cannot be loaded because it is too large. " +
+                $"Maximum table capacity: \"{MaxDataDimension}\"");
+
+            return;
+        }
+
+        DataTable dataTableMatrixA = new();
+        DataTable dataTableVectorB = new();
+
+        for (var i = 0; i < sole.Dimension; i++)
+            dataTableMatrixA.Columns.Add((i + 1).ToString(), typeof(double)).DefaultValue = default(double);
+
+        dataTableVectorB.Columns.Add("1", typeof(double)).DefaultValue = default(double);
+
+        for (var i = 0; i < sole.Dimension; i++)
+        {
+            _ = dataTableMatrixA.Rows.Add();
+            _ = dataTableVectorB.Rows.Add();
+        }
+
+        for (var i = 0; i < sole.Dimension; i++)
+        {
+            for (var j = 0; j < sole.Dimension; j++)
+                dataTableMatrixA.Rows[i][j] = sole.A[i, j];
+
+            dataTableVectorB.Rows[i][0] = sole.B[i];
+        }
+
+        _currentDataDimension = sole.Dimension;
+
+        DataTableMatrixA = dataTableMatrixA;
+        DataTableVectorB = dataTableVectorB;
+    }
+
+    private void ClearData()
+    {
+        DataTable dataTableMatrixA = new();
+        DataTable dataTableVectorB = new();
+
+        dataTableMatrixA.Columns.Add("1", typeof(double)).DefaultValue = default(double);
+        dataTableVectorB.Columns.Add("1", typeof(double)).DefaultValue = default(double);
+
+        _ = dataTableMatrixA.Rows.Add();
+        _ = dataTableVectorB.Rows.Add();
+
+        _currentDataDimension = 1;
+
+        DataTableMatrixA = dataTableMatrixA;
+        DataTableVectorB = dataTableVectorB;
+    }
+
+    private void ChangeDataDimension(int dimensionCount, bool isExpanding)
+    {
+        if (((isExpanding is true) && ((_currentDataDimension + dimensionCount) > MaxDataDimension)) ||
+            ((isExpanding is false) && ((_currentDataDimension - dimensionCount) < MinDataDimension)))
+            return;
+
+        DataTable dataTableMatrixA = DataTableMatrixA.Copy();
+        DataTable dataTableVectorB = DataTableVectorB.Copy();
+
+        if (isExpanding is true)
+        {
+            for (var i = 0; i < dimensionCount; i++)
+            {
+                _currentDataDimension++;
+
+                dataTableMatrixA.Columns
+                    .Add(new DataColumn(_currentDataDimension.ToString(), typeof(double)) { DefaultValue = default(double) });
+
+                _ = dataTableMatrixA.Rows.Add();
+                _ = dataTableVectorB.Rows.Add();
+            }
+        }
+        else
+        {
+            for (var i = 0; i < dimensionCount; i++)
+            {
+                _currentDataDimension--;
+
+                dataTableMatrixA.Columns.RemoveAt(_currentDataDimension);
+
+                dataTableMatrixA.Rows.RemoveAt(_currentDataDimension);
+                dataTableVectorB.Rows.RemoveAt(_currentDataDimension);
+            }
+        }
+
+        DataTableMatrixA = dataTableMatrixA;
+        DataTableVectorB = dataTableVectorB;
+    }
+
+    private void ResetData()
+    {
+        ClearData();
+        ChangeDataDimension(4, true);
+    }
+
+    private Sole GetSoleFromData()
+    {
+        double[,] a = new double[_currentDataDimension, _currentDataDimension];
+        double[] b = new double[_currentDataDimension];
+
+        for (var i = 0; i < _currentDataDimension; i++)
+            for (var j = 0; j < _currentDataDimension; j++)
+                a[i, j] = (double)DataTableMatrixA.Rows[i][j];
+
+        for (var i = 0; i < _currentDataDimension; i++)
+            b[i] = (double)DataTableVectorB.Rows[i][0];
+
+        return new Sole(a, b);
+    }
+
+    private void SetSolutionSet(SoleSolvingResults solvingResults)
+    {
+        if (solvingResults.SolutionSet.Any() is true)
+        {
+            StringBuilder solutionSetStringBuilder = new();
+
+            for (var i = 0; i < _currentDataDimension; i++)
+                _ = solutionSetStringBuilder.Append($"x{i + 1}={solvingResults.SolutionSet[i]};   ");
+
+            SolutionSet = solutionSetStringBuilder.ToString();
+        }
+        else
+            SolutionSet = "This system of linear equations has no unique solutions.";
+    }
+}
