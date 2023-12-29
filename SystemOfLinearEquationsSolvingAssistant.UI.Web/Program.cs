b5ba0dbf2050ecc368d26b352b96785a702dcb22
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2023-12-29 17:48:10 +0300

    feat(web): add application entry point

--- SystemOfLinearEquationsSolvingAssistant.UI.Web/Program.cs
+++ SystemOfLinearEquationsSolvingAssistant.UI.Web/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using SystemOfLinearEquationsSolvingAssistant.Dependencies;
+using SystemOfLinearEquationsSolvingAssistant.UI.Web.DependencyRegistrators;
+using SystemOfLinearEquationsSolvingAssistant.UI.Web.Services.Interfaces;
+using SystemOfLinearEquationsSolvingAssistant.UI.Web.Views;
+
+ToolsRegistrator.Register();
+ServicesRegistrator.Register();
+ViewModelsRegistrator.Register();
+ViewsRegistrator.Register();
+
+IHttpServerManagerService httpServerManagerService = DependenciesContainer.Resolve<IHttpServerManagerService>()!;
+httpServerManagerService.SetDefaultView<MainView>();
+httpServerManagerService.StartServer();
+
+Console.ReadLine();
